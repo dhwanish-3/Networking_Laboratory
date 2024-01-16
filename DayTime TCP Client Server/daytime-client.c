caa5fbb331ec3d13ef8088f2ee25fc86d3093b37
dhwanish-3 <108540722+dhwanish-3@users.noreply.github.com>
2024-01-16 21:47:57 +0530

    TCP Daytime - Exp 1 added

---
+++ DayTime TCP Client Server/daytime-client.c
@@ -0,0 +1,60 @@
+#include <stdio.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <errno.h>
+#include <string.h>
+#include <sys/types.h>
+#include <time.h>
+
+#define MAXLINE 1024
+#define LISTENQ 10
+
+typedef struct sockaddr SA;
+
+int main (int argc, char **argv){
+    int sockfd, n;
+    char recvline [MAXLINE + 1];
+    struct sockaddr_in servaddr;
+    int port;
+
+    if(argc != 3){
+        printf("Usage: a.out <IPaddress> <port no.>");
+    }
+    if ((sockfd = socket(AF_INET, SOCK_STREAM, 0)) < 0){ // Creates a TCP Socket
+            printf("Socket Error");
+    }
+
+    port = atoi(argv[2]); // ascii to integer conversion
+
+    bzero(&servaddr, sizeof(servaddr)); // fills servaddr with zeros.
+
+    servaddr.sin_family = AF_INET;
+    servaddr.sin_port = htons(port);
+
+    if (inet_pton(AF_INET, argv[1], &servaddr.sin_addr) <= 0){
+    // pton = presentatin to network
+        printf("inet_pton error for %s", argv[1]);
+    }
+
+    if (connect(sockfd, (SA *) &servaddr, sizeof(servaddr)) < 0){
+     // Establishes a TCP Connnection
+        printf("Connect Error.\n");
+    }
+
+    while ((n=read(sockfd, recvline, MAXLINE)) > 0){
+    // reads server reply.
+        recvline[n] =0;
+
+        if (fputs(recvline, stdout) == EOF){
+            printf("fputs error.\n");
+        }
+    }
+
+    if (n <0){
+        printf("read error");
+    }
+    return 0;
+}
---
+++ DayTime TCP Client Server/daytime-server.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <errno.h>
+#include <string.h>
+#include <sys/types.h>
+#include <time.h>
+
+#define MAXLINE 1024
+#define LISTENQ 10
+
+typedef struct sockaddr SA;
+
+int main(int argc, char **argv){
+    int listenfd, connfd;
+    struct sockaddr_in servaddr, cliaddr;
+    char buff [MAXLINE];
+    time_t ticks;
+    int port;
+    socklen_t len;
+
+    listenfd = socket(AF_INET, SOCK_STREAM, 0); // Creates a TCP Socket
+
+    port = atoi(argv[1]);
+    bzero(&servaddr, sizeof(servaddr));
+    servaddr.sin_family = AF_INET;
+    servaddr.sin_addr.s_addr = htonl(INADDR_ANY);
+    servaddr.sin_port = htons(port);
+
+    bind(listenfd, (SA *) &servaddr, sizeof(servaddr));
+    printf("Server is waiting connection at port %d\t\n", port);
+    listen(listenfd, LISTENQ);
+
+    for ( ; ; ) {
+        len = sizeof(cliaddr);
+        connfd = accept(listenfd, (SA *) &cliaddr, &len);
+        printf("Connection from %s, port %d\n", inet_ntop(AF_INET,
+        &cliaddr.sin_addr.s_addr, buff, sizeof(buff)), ntohs(cliaddr.sin_port));
+        ticks = time(NULL);
+        snprintf(buff, sizeof(buff), "%.24s\r\n", ctime(&ticks));
+        write(connfd, buff, strlen(buff));
+        close(connfd);
+    }
+    return 0;
+}
