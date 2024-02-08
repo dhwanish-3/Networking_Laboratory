44e2575c4222da2a4dbc1d4f03573287415e619f
dhwanish-3 <108540722+dhwanish-3@users.noreply.github.com>
2024-02-08 21:45:18 +0530

    New scripts for running server and client

--- run_server.sh
+++ run_server.sh
@@ -5,33 +5,21 @@ file=$(find . -name "*.c" -type f -printf '%T@ %p\n' | sort -n | tail -1 | cut -
 
 # Get the directory and filename without extension
 dir=$(dirname "$file")
-echo $dir
 filename=$(basename "$file" | cut -f 1 -d '.')
-echo "last modified file: $filename"
 
 # change directory
 echo "changing directory to $dir"
 cd "$dir"
 
-# compiling the files using makefile
+# compiling the files using 
 make
 
-# getting server object name
 # removing the characters after _ in filename
 base=$(echo $filename | cut -f 1 -d '_')
 
-# appending _server and _client to the base name
+# appending _server to the base name
 server=$base"_server"
-client=$base"_client"
 
 # run the server
-echo "running server $server in the background"
-"./$server" &
-
-# getting the ip address
-ipaddress=$(hostname -I | cut -f1 -d' ')
-
-# run the client
-echo "running client $client"
-
-"./$client" "$ipaddress"
+echo "running server $server"
+"./$server"
